8ea32e9b04cceeb7b9a5c561589830b1283bb41d
Ahmed Maher <ahmedmaher.sw@gmail.com>
2024-05-11 20:12:52 +0300

    feat(list): implement list fragment/viewmodel

---
+++ feature/list/src/main/kotlin/com/example/list/presentation/view/ListFragment.kt
@@ -0,0 +1,83 @@
+package com.example.list.presentation.view
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import com.example.di.DaggerCoreComponent
+import com.example.list.R
+import com.example.list.databinding.FragmentListBinding
+import com.example.list.di.DaggerListComponent
+import com.example.list.presentation.adapter.UniversitiesAdapter
+import com.example.list.presentation.viewmodel.ListViewModel
+import com.example.ui.App
+import com.example.ui.extension.observe
+import com.example.ui.extension.viewBinding
+import javax.inject.Inject
+
+class ListFragment : Fragment(R.layout.fragment_list) {
+    private val binding by viewBinding(FragmentListBinding::bind)
+    @Inject
+    lateinit var factory : ViewModelProvider.Factory
+
+    private val viewModel: ListViewModel by lazy { ViewModelProvider(this, factory)[ListViewModel::class.java] }
+    private val adapter by lazy { UniversitiesAdapter() }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+        with(viewModel) { observe(uiState, ::renderState) }
+    }
+
+    override fun onAttach(context: Context) {
+        initDaggerComponent()
+        super.onAttach(context)
+    }
+
+    private fun initDaggerComponent() {
+        DaggerListComponent
+            .builder()
+            .coreComponent(App.coreComponent(requireContext()))
+            .build()
+            .inject(this)
+    }
+
+    private fun initUI() {
+        binding.rvUniversities.adapter = adapter
+        adapter.select = { item -> // TODO: navigate to details
+        }
+    }
+
+    private fun renderState(uiState: ListUIState) {
+        when (uiState) {
+            is ListUIState.Loading -> showLoading()
+            is ListUIState.Empty -> resetAdapter()
+            is ListUIState.Success -> {
+                hideLoading()
+                adapter.submitList(uiState.list)
+            }
+
+            is ListUIState.Fail -> {
+                hideLoading()
+                resetAdapter()
+                Toast.makeText(context, uiState.throwable.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun resetAdapter() {
+        adapter.submitList(null)
+    }
+
+    private fun showLoading() {
+        binding.progress.isVisible = true
+    }
+
+    private fun hideLoading() {
+        binding.progress.isVisible = false
+    }
+}
---
+++ feature/list/src/main/kotlin/com/example/list/presentation/viewmodel/ListViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.list.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.list.domain.usecase.GetUniversitiesUseCase
+import com.example.list.presentation.view.ListUIState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+class ListViewModel @Inject constructor(private val getUniversitiesUseCase: GetUniversitiesUseCase) : ViewModel() {
+    private val _uiState: MutableStateFlow<ListUIState> = MutableStateFlow(ListUIState.Loading)
+    val uiState: StateFlow<ListUIState> = _uiState.asStateFlow()
+
+    init {
+        getUniversities()
+    }
+
+    private fun getUniversities() {
+        viewModelScope.launch {
+            getUniversitiesUseCase.execute().apply {
+                onSuccess { list -> ListUIState.Success(list).also { _uiState.value = it } }
+                onFailure { throwable -> ListUIState.Fail(throwable).also { _uiState.value = it } }
+            }
+        }
+    }
+}
